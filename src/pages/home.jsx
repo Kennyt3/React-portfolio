f63e6c63f8fb8af07cfb104b15ff190568f22fb8
Taiwo Mercy <kennyt341@gmail.com>
2023-01-05 11:12:46 +0100

    Added some dependencies

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -5,6 +5,7 @@ import { useProvider } from '../components/context'
 import Loading from '../components/loading'
 
 import Hero from '../components/hero'
+
 const Home = () => {
   const { lightmode, loading } = useProvider()
   return (
@@ -17,7 +18,13 @@ const Home = () => {
             lightmode ? 'bg-primary600' : 'bg-white'
           }`}
         >
-          <Snowfall snowflakeCount={200} speed={[0, 0.5]} wind={[-0.5, 1]} />
+          <Snowfall
+            className='text-primary800'
+            snowflakeCount={200}
+            speed={[0, 0.5]}
+            wind={[-0.5, 1]}
+            color={!lightmode ? 'rgb(183, 92, 255)' : 'rgb(245, 245, 220)'}
+          />
           <div className='max-w-7xl mx-auto px-3'>
             <div>
               <Navbar />
