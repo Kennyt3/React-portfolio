d290968826daffd6d50e7a6a4a5a8ccc5c0070d9
Taiwo Mercy <kennyt341@gmail.com>
2023-01-06 07:30:08 +0100

    made changes to the frontpage

--- src/components/loading.jsx
+++ src/components/loading.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { useEffect } from 'react'
 import AOS from 'aos'
 import 'aos/dist/aos.css'
+import Snowfall from 'react-snowfall'
 
 const Loading = () => {
   useEffect(() => {
@@ -10,6 +11,12 @@ const Loading = () => {
   }, [])
   return (
     <div className='bg-primary100 text-primary800 h-screen'>
+      <Snowfall
+        radius={[0.5, 1.5]}
+        snowflakeCount={200}
+        speed={[0, 0.5]}
+        wind={[-0.5, 1]}
+      />
       <div className='max-w-7xl h-full  mx-auto px-3 flex flex-col items-center gap-4 justify-center'>
         <h2
           className='text-3xl design  text-primary500 text-center sm:text-6xl'
