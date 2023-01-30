0368b7cc3e283371960792338d2024e704dda6e0
Taiwo Mercy <kennyt341@gmail.com>
2023-01-30 11:43:03 +0100

    Added a fashion ecommerce app to my projects

--- src/projectData.js
+++ src/projectData.js
@@ -3,6 +3,7 @@ import image3 from './assets/image3.png'
 import image4 from './assets/image4.png'
 import image1 from './assets/image1.png'
 import image5 from './assets/image5.png'
+import image6 from './assets/image6.png'
 
 export const projectData = [
   {
@@ -37,8 +38,17 @@ export const projectData = [
   },
   {
     id: 4,
-    name: 'Teamkernel',
+    name: 'Boma Fashion Store',
     pic: image4,
+    details: 'An fashion ecommerce built using React, Tailwind and Javascript',
+    live: 'https://bomafashion.netlify.app/',
+    github: 'https://github.com/Kennyt3/BOMA',
+    aos: 'fade-right',
+  },
+  {
+    id: 5,
+    name: 'Teamkernel',
+    pic: image5,
     details:
       'A review web application, to submit reviews on services built using Reactjs aand styled components',
     live: 'https://teamkernel.netlify.app/',
@@ -46,9 +56,9 @@ export const projectData = [
     aos: 'fade-down-left',
   },
   {
-    id: 5,
+    id: 6,
     name: "Mercy's To-do",
-    pic: image5,
+    pic: image6,
     details:
       'A To-do web application for creating to do lists, built using Html, CSS and Javascript',
     live: 'https://mercystodo.netlify.app/',
