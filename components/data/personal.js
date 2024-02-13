232207100c38cb8557791450234d745db28c7b73
Mercy <kennyt341@gmail.com>
2024-02-13 17:07:45 +0100

    j

--- components/data/personal.js
+++ components/data/personal.js
@@ -4,7 +4,74 @@ export const work = [
   { org: 'Women Techsters Fellowship', date: '2023-01 – 2023-06', details: '' },
 ]
 export const ally = [
-  { name: 'BeeSage', github: '', live: '' },
-  { name: 'United States of Africa', github: '', live: '' },
-  { name: 'Women Techsters Fellowship', github: '2023-01 – 2023-06', live: '' },
+  {
+    id: 1,
+    name: 'BLOG CMS',
+    pic: image7,
+    details:
+      'A User Friendly Blog cms built using Next js, Node js, mongodb and aws-sdk',
+    live: 'https://wryt-it.vercel.app/',
+    github: 'https://github.com/Kennyt3/WrytIt',
+    aos: 'fade-up',
+  },
+  {
+    id: 2,
+    name: 'Quizzard',
+    pic: image1,
+    details:
+      'A web quiz application built using Reactjs, tailwindcss, and quiz api',
+    live: 'https://quizzardapp.netlify.app/',
+    github: 'https://github.com/Kennyt3/Quizzard',
+    aos: 'fade-up-left',
+  },
+  {
+    id: 3,
+    name: 'Jokesbot',
+    pic: image2,
+    details:
+      'A random joke telling web app built using Reactjs tailwindcss, web speech api, official jokes api',
+    live: 'https://jokesbot.netlify.app/',
+    github: 'https://github.com/Kennyt3/jokesbot',
+    aos: 'fade-up-right',
+  },
+  {
+    id: 4,
+    name: 'TMK Calculator',
+    pic: image3,
+    details:
+      'A web application used to do basic mathematical calculations, built using React, Tailwind and Javascript',
+    live: 'https://tmkcalculator.netlify.app/',
+    github: 'https://github.com/Kennyt3/Calculator',
+    aos: 'fade-up',
+  },
+  {
+    id: 5,
+    name: 'Boma Fashion Store',
+    pic: image4,
+    details:
+      'A fashion ecommerce app built using React, Tailwind and Javascript',
+    live: 'https://bomafashion.netlify.app/',
+    github: 'https://github.com/Kennyt3/BOMA',
+    aos: 'fade-right',
+  },
+  {
+    id: 6,
+    name: 'REVIEW ME',
+    pic: image5,
+    details:
+      'A review web application, to submit reviews on services built using Reactjs aand styled components',
+    live: 'https://teamkernel.netlify.app/',
+    github: 'https://github.com/Kennyt3/TeamKernel',
+    aos: 'fade-down-left',
+  },
+  {
+    id: 7,
+    name: '2-do Planner',
+    pic: image6,
+    details:
+      'A To-do web application for creating to do lists, built using Html, CSS and Javascript',
+    live: 'https://mercystodo.netlify.app/',
+    github: 'https://github.com/Kennyt3/Icecream',
+    aos: 'fade-down-right',
+  },
 ]
