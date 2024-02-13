58e7e3746461546bc3b78c2a91301ddfecca974d
Mercy <kennyt341@gmail.com>
2024-02-13 17:06:12 +0100

    j

--- components/data/personal.js
+++ components/data/personal.js
@@ -3,3 +3,8 @@ export const work = [
   { org: 'United States of Africa', date: '2022-08 – 2023-01', details: '' },
   { org: 'Women Techsters Fellowship', date: '2023-01 – 2023-06', details: '' },
 ]
+export const ally = [
+  { name: 'BeeSage', github: '', live: '' },
+  { name: 'United States of Africa', github: '', live: '' },
+  { name: 'Women Techsters Fellowship', github: '2023-01 – 2023-06', live: '' },
+]
--- components/portfolio.js
+++ components/portfolio.js
@@ -7,19 +7,14 @@ const Portfolio = (page) => {
         <div className='mx-auto mt-10 md:mt-0 h-full relative'>
           <div className='doro dub h-full'>
             <div className='w-full mx-auto md:h-[80vh] rounded-[20px] border-[#4db5ff] border-solid border-[5px] px-5 md:gap-10'>
-              <div className='flex gap-10 justify-center items-center'>
-                <p>All</p>
-                <p>Next js</p>
-                <p>React js</p>
-                <p>Node js</p>
+              <div className='flex gap-10 justify-center items-center mt-5 bg-[#4db5ff] w-fit mx-auto rounded-[10px] px-5 py-2'>
+                <button className='hover:text-[#1f1f38]'>All</button>
+                <button className='hover:text-[#1f1f38]'>Next js</button>
+                <button className='hover:text-[#1f1f38]'>React js</button>
+                <button className='hover:text-[#1f1f38]'>Node js</button>
               </div>
               <div className='px-3 md:max-h-[90%] overflow-scroll'>
                 <div className=' grid items-center grid-cols-2 gap-10 '>
-                  <div className='bg-[#4db5ff] w-full my-5 md:my-10 rounded-[10px] md:min-h-40 p-3 text-[#1f1f38]  basis-1/2 flex flex-col'></div>
-                  <div className='bg-[#4db5ff] w-full my-5 md:my-10 rounded-[10px] md:min-h-40 p-3 text-[#1f1f38]  basis-1/2 flex flex-col'></div>
-                  <div className='bg-[#4db5ff] w-full my-5 md:my-10 rounded-[10px] md:min-h-40 p-3 text-[#1f1f38]  basis-1/2 flex flex-col'></div>
-                  <div className='bg-[#4db5ff] w-full my-5 md:my-10 rounded-[10px] md:min-h-40 p-3 text-[#1f1f38]  basis-1/2 flex flex-col'></div>
-                  <div className='bg-[#4db5ff] w-full my-5 md:my-10 rounded-[10px] md:min-h-40 p-3 text-[#1f1f38]  basis-1/2 flex flex-col'></div>
                   <div className='bg-[#4db5ff] w-full my-5 md:my-10 rounded-[10px] md:min-h-40 p-3 text-[#1f1f38]  basis-1/2 flex flex-col'></div>
                 </div>
               </div>
