310ccad75af395c2a0ae70a4a3e6d0441a5980e7
Mercy <kennyt341@gmail.com>
2024-08-05 17:25:39 +0100

    me

--- components/body.js
+++ components/body.js
@@ -3,6 +3,8 @@ import { FaGithub } from 'react-icons/fa6'
 import { FaLinkedin } from 'react-icons/fa6'
 import { FaXTwitter } from 'react-icons/fa6'
 import { FaInstagram } from 'react-icons/fa6'
+import Header from './Header'
+export const row = ['ABOUT', 'EXPERIENCE', 'PROJECTS']
 export const icon = [
   {
     icon: <FaGithub className='icon' size={20} />,
@@ -42,19 +44,20 @@ const Body = () => {
   }, [])
 
   return (
-    <div className='relative group/spotlight'>
+    <div className='relative'>
+      <Header />
       <div
-        className='pointer-events-none fixed inset-0 z-30 transition duration-300 lg:absolute'
+        className='pointer-events-none fixed inset-0 z-30  lg:absolute'
         style={{
           background:
             'radial-gradient(600px at 788px 600px, rgba(29, 78, 216, 0.15), transparent 100%)',
-          left: mousePosition.x - 900,
-          top: mousePosition.y - 300,
+          // left: mousePosition.x - 900,
+          // top: mousePosition.y - 300,
         }}
       ></div>
       <div className='mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0'>
         <div className='lg:flex lg:justify-between lg:gap-4'>
-          <header className='lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-40'>
+          <section className='lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24'>
             <div>
               <h1 className='text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl'>
                 <a href='/'>Mercy K. Taiwo</a>
@@ -63,48 +66,61 @@ const Body = () => {
                 Frontend Engineer
               </h2>
               <p className='mt-4 max-w-xs leading-normal'>
-                I build pixel-perfect, engaging, and accessible digital
-                experiences.
+                {/* catch phrase along the lines of I enable business growth by
+                building web sites that connects to target audience but more
+                creative */}
               </p>
               <nav
                 className='nav hidden lg:block'
-                aria-label='In-link jump links'
+                aria-label='In-page jump links'
               >
-                <ul
-                  className='ml-1 mt-8 flex items-center'
-                  aria-label='Social media'
-                >
-                  {icon.map((item, index) => (
+                <ul className='mt-16 w-max'>
+                  {row.map((item, index) => (
                     <li key={index}>
-                      <a
-                        class='block hover:text-slate-200'
-                        href={item.link}
-                        target='_blank'
-                        rel='noreferrer noopener'
-                        aria-label='Goodreads (opens in a new tab)'
-                        title={item.name}
-                      >
-                        {item.icon}
+                      <a className='group flex items-center py-3' href='#about'>
+                        <span className='nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none'></span>
+                        <span className='nav-text text-xs font-bold uppercase tracking-widest text-slate-500 group-hover:text-slate-200 group-focus-visible:text-slate-200'>
+                          {item}
+                        </span>
                       </a>
                     </li>
                   ))}
                 </ul>
               </nav>
             </div>
-          </header>
-          <main id='content' class='pt-24 lg:w-1/2 lg:py-24'>
+            <ul
+              className='ml-1 mt-8 flex items-center'
+              aria-label='Social media'
+            >
+              {icon.map((item, index) => (
+                <li key={index} className='mr-5 text-xs shrink-0'>
+                  <a
+                    className='block hover:text-slate-200'
+                    href={item.link}
+                    target='_blank'
+                    rel='noreferrer noopener'
+                    aria-label='Goodreads (opens in a new tab)'
+                    title={item.name}
+                  >
+                    {item.icon}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </section>
+          <main id='content' className='pt-24 lg:w-1/2 lg:py-24'>
             <section
               id='about'
-              class='mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24'
+              className='mb-16 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24'
               aria-label='About me'
             >
-              <div class='sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0'>
-                <h2 class='text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only'>
+              <div className='sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0'>
+                <h2 className='text-sm font-bold uppercase tracking-widest text-slate-200 lg:sr-only'>
                   About
                 </h2>
               </div>
               <div>
-                <p class='mb-4'>
+                <p className='mb-4'>
                   My journey into software development began during the economic
                   hardships brought on by COVID-19. As an undergraduate student
                   in my penultimate year, it became challenging for my parents
@@ -112,7 +128,7 @@ const Body = () => {
                   bridge the gap before I could practice law, I decided to learn
                   a skill that would sustain me.
                 </p>
-                <p class='mb-4'>
+                <p className='mb-4'>
                   In furtherance of this cause, I joined the Women Techsters
                   Fellowship, a prestigious program sponsored by Microsoft,
                   where I graduated summa cum laude and was honored as the Best
@@ -121,7 +137,7 @@ const Body = () => {
                   excellence. I have taken a number of other certification
                   courses as well as participated in a mentorship program
                 </p>
-                <p class='mb-4'>
+                <p className='mb-4'>
                   Professionally, I've single-handedly built Beesage's current
                   webshop, integrating the Stripe API and a blog CMS tool.
                   Additionally, I've developed various websites for clients,
@@ -130,29 +146,6 @@ const Body = () => {
               </div>
             </section>
           </main>
-          <div>
-            <button
-              class='hover:-text-teal-300 absolute bottom-0 right-0 inline-flex items-center px-2 py-4 font-medium text-slate-400 hover:-translate-y-2 focus-visible:text-teal-300'
-              type='button'
-              aria-haspopup='dialog'
-              aria-expanded='false'
-              aria-controls='radix-:R4l6:'
-              data-state='closed'
-            >
-              <span class='sr-only'>Click to time travel</span>
-              <img
-                alt='Spinning Tardis from Doctor Who'
-                loading='lazy'
-                width='100'
-                height='86'
-                decoding='async'
-                data-nimg='1'
-                style={{ color: 'transparent' }}
-                srcset='/_next/image?url=%2Fimages%2Ftardis%2Frotate.gif&amp;w=128&amp;q=75 1x, /_next/image?url=%2Fimages%2Ftardis%2Frotate.gif&amp;w=256&amp;q=75 2x'
-                src='/_next/image?url=%2Fimages%2Ftardis%2Frotate.gif&amp;w=256&amp;q=75'
-              />
-            </button>
-          </div>
         </div>
       </div>
     </div>
